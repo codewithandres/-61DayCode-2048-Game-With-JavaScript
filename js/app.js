591df5b521631561a1e5d37ef9abdfe52e814916
Dize dev <axiozweb2022@gmail.com>
2024-06-04 23:24:18 -0500

    creamos la funcion para iniciar el juego

---
+++ js/app.js
@@ -0,0 +1,33 @@
+
+let grid, score, timer, time, gameStarted;
+
+document.getElementById('new-game').addEventListener('click', initGame);
+document.getElementById('tryangan').addEventListener('click', initGame)
+
+document.addEventListener('keydown', handleInput);
+
+const handleInput = () => {
+
+    grid = createEmptyGrid();
+    score = 0;
+    time = 0;
+    timer = 0;
+    gameStarted = false;
+    updateScore();
+    addRamdownTitle();
+    drawGrid();
+    hideGameOverMessage();
+}
+
+const pauseButton = document.getElementById('pause-game');
+let isPaused = false;
+
+pauseButton.addEventListener('click', () => {
+
+    if (isPaused) resumeGame()
+    else pausedGame();
+});
+
+const pausedGame = () => {
+
+};
